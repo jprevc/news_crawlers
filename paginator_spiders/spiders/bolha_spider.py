565e0b74d3609558e7ec81e984d48ec14d8aa2aa
jprevc <jost.prevc@gmail.com>
2021-02-09 20:37:05 +0100

    added bolha_spider, which traverses pagination links to found all items' hrefs and corresponding prices

---
+++ paginator_spiders/spiders/bolha_spider.py
@@ -0,0 +1,34 @@
+import os
+import scrapy
+import yaml
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+class BolhaSpider(scrapy.Spider):
+    name = "bolha"
+    home_path = os.environ.get('BOLHA_HOME', os.path.dirname(__file__))
+
+    # get configuration data
+    with open(os.path.join(home_path, 'bolha_configuration.yaml'), 'r') as f:
+        config_data = yaml.load(f)
+
+    start_urls = config_data['urls']
+
+    def parse(self, response):
+        items = response.xpath('//*[text() = "Oglasi na bolha.com"]/following-sibling::ul/li[@data-href]')
+        for item in items:
+            yield {
+                'url': response.urljoin(item.attrib['data-href']),
+                'price': item.xpath('.//li[@class="price-item"]/strong//text()').get().strip(),
+            }
+
+        next_page = response.xpath('//a[@class="Pagination-link"][text() = "Naslednja\xa0"]')
+        if next_page:
+            yield response.follow(next_page.attrib['href'], callback=self.parse)
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess(get_project_settings())
+
+    process.crawl('bolha')
+    process.start()  # the script will block here until the crawling is finished
