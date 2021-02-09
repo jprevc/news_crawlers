f3e0fae2647f89d753792b6fe8f44362b6517011
jprevc <jost.prevc@gmail.com>
2021-02-09 20:34:33 +0100

    ROBOTSTXT_OBEY is set to False, otherwise scrapy does not work with bolha.com

--- paginator_spiders/settings.py
+++ paginator_spiders/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'paginator_spiders.spiders'
 #USER_AGENT = 'paginator_spiders (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
