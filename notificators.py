8626152c3c59566d2f8f2530bb7ccf7cfc076b6f
jprevc <jost.prevc@gmail.com>
2021-08-09 22:24:38 +0200

    added PushoverNotificator implementation of NotificatorBase

--- notificators.py
+++ notificators.py
@@ -1,18 +1,43 @@
 from abc import ABC, abstractmethod
 import os
 import smtplib
+import requests
 
 
 class NotificatorBase(ABC):
+
     def __init__(self, recipients):
         self.recipients = recipients
 
     @abstractmethod
     def send(self, subject, message):
+        """
+        Sends notification.
+
+        :param subject: Subject (title) of notification.
+        :type subject: str
+
+        :param message: Message content.
+        :type message: str
+        """
         pass
 
 
-class EmailNotificator(Notificator):
+class EmailNotificator(NotificatorBase):
+    """
+    Email notification implementation.
+
+    This implementation uses gmail's SMTP server to send notifications as emails to users.
+
+    This implementation requires user credentials (email address and password) to be
+    stored in environment variables as EMAIL_PASS and EMAIL_USER. To avoid storing your
+    password in system variables, it is advised to generate a special password to be used only
+    for this application. This can be done here:
+
+    https://myaccount.google.com/apppasswords and generate a new app password for your account.
+
+    This password can then safely be stored under EMAIL_PASS variable.
+    """
 
     def send(self, subject, message):
         """
@@ -37,3 +62,37 @@ class EmailNotificator(Notificator):
             msg = f'Subject: {subject}\n\n{message}'
 
             smtp.sendmail(email_user, self.recipients, msg.encode('utf8'))
+
+
+class PushoverNotificator(NotificatorBase):
+    """
+    Pushover notification implementation.
+
+    This implementation uses Pushover (https://pushover.net/) to send push notifications to the
+    recipients' mobile phones.
+
+    This implementation requires 'PUSHOVER_APP_TOKEN' variable to be stored in environment variables.
+    Value of this variable should be a pushover application token, which can be generated here:
+
+    https://pushover.net/apps/build
+
+    :param recipients: Pushover user keys of recipients to which push notification should be sent.
+    :type recipients: list
+    """
+
+    def send(self, subject, message):
+        """
+        Sends a pushover notification.
+
+        :param subject: Subject of push notification.
+        :type subject: str
+
+        :param message: Push notification message.
+        :type message: str
+        """
+        session = requests.Session()
+        for user_key in self.recipients:
+            payload = {"token": os.environ.get('PUSHOVER_APP_TOKEN'),
+                       "user": user_key,
+                       "message": message}
+            session.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
