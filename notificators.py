739925ef29e616d7ff538b5e6060587fb2a3a476
jprevc <jost.prevc@gmail.com>
2021-08-13 22:16:22 +0200

    added send_items method to notificators, configuration parameters such as credentials must now be passed as constructor

--- notificators.py
+++ notificators.py
@@ -6,7 +6,9 @@ Contains various Notificator implementations.
 from abc import ABC, abstractmethod
 import os
 import smtplib
+
 import requests
+import numpy as np
 
 
 class NotificatorBase(ABC):
@@ -19,7 +21,7 @@ class NotificatorBase(ABC):
         self.recipients = recipients
 
     @abstractmethod
-    def send(self, subject, message):
+    def send_text(self, subject, message):
         """
         Sends notification.
 
@@ -29,6 +31,18 @@ class NotificatorBase(ABC):
         :param message: Message content.
         :type message: str
         """
+
+    def send_items(self, subject, items, item_format, send_separate=False):
+        text = ""
+        for item in items:
+            item_text = item_format.format(**item)
+            if send_separate:
+                self.send_text(subject, item_text)
+            else:
+                text += item_text
+
+        if not send_separate:
+            self.send_text(subject, text)
 
 
 class EmailNotificator(NotificatorBase):
@@ -46,8 +60,15 @@ class EmailNotificator(NotificatorBase):
 
     This password can then safely be stored under EMAIL_PASS variable.
     """
+    def __init__(self, recipients, email_user, email_password):
+        super().__init__(recipients)
+        self._email_user = email_user
+        self._email_password = email_password
 
-    def send(self, subject, message):
+    def _get_smtp_session(self):
+        return smtplib.SMTP('smtp.gmail.com', 587)
+
+    def send_text(self, subject, message):
         """
         Sends email message.
 
@@ -57,10 +78,10 @@ class EmailNotificator(NotificatorBase):
         :param message: Email message content.
         :type message: str
         """
-        email_password = os.environ.get('EMAIL_PASS')
-        email_user = os.environ.get('EMAIL_USER')
+        email_password = self._email_password
+        email_user = self._email_user
 
-        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        with self._get_smtp_session() as smtp:
             smtp.ehlo()
             smtp.starttls()
             smtp.ehlo()
@@ -87,8 +108,15 @@ class PushoverNotificator(NotificatorBase):
     :param recipients: Pushover user keys of recipients to which push notification should be sent.
     :type recipients: list
     """
+    def __init__(self, recipients, app_token):
+        super().__init__(recipients)
+        self._app_token = app_token
 
-    def send(self, subject: str, message: str):
+    @staticmethod
+    def _open_session():
+        return requests.Session()
+
+    def send_text(self, subject: str, message: str):
         """
         Sends a pushover notification.
 
@@ -96,10 +124,31 @@ class PushoverNotificator(NotificatorBase):
 
         :param message: Push notification message.
         """
-        session = requests.Session()
+        session = self._open_session()
         for user_key in self.recipients:
-            payload = {"token": os.environ.get('PUSHOVER_APP_TOKEN'),
+            payload = {"token": self._app_token,
                        "user": user_key,
                        "title": subject,
                        "message": message}
             session.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
+
+    def send_items(self, subject, items, item_format, send_separate=False):
+        if send_separate:
+            # here it is assumed, that any single item's text will not exceed 1024 character limitation
+            super().send_items(subject, items, item_format, send_separate=True)
+        else:
+            # pushover messages are limited to 1024 characters. Items need to be divided to separate text blocks and
+            # sent separately if text would exceed that limit
+
+            # get list of indexes, where each index corresponds to group in which item should be sent. Indexes are
+            # selected according to message size limitation, such that no message will exceed 1024 characters
+            item_groups = np.cumsum([len(item_format.format(**item)) for item in items]) // 1025
+
+            # get list of text messages for each group
+            groups_txt_blocks = [""] * (max(item_groups) + 1)
+            for item, group in zip(items, item_groups):
+                groups_txt_blocks[group] += item_format.format(**item)
+
+            # send messages in separate notifications
+            for txt_block in groups_txt_blocks:
+                self.send_text(subject, txt_block)
