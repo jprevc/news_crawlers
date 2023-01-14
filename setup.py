e0a5df3b303b31ea3a937ddcbbbd4d47ea8ee126
jprevc <jost.prevc@gmail.com>
2023-01-14 12:36:09 +0100

    installation is now done with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 """
-Setup script
+Setup script.
 """
 
 from setuptools import setup
 
-setup(
-    name="news_crawlers",
-    version="1.0",
-    description="News crawlers",
-    author="Jost Prevc",
-    author_email="jost.prevc@gmail.com",
-    packages=["news_crawlers"],
-    install_requires=[
-        "scrapy",
-        "PyYAML",
-        "requests",
-        "numpy",
-        "parameterized",
-        "pytest",
-    ],
-)
+setup()
